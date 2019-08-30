22350bf4898f4af5b9adc0a4ecbf4c116411d81a
Yikai Zhao <zdwzhaoyikai@gmail.com>
2019-08-29 23:20:19 -0500

    added docs and updated faster funcs

--- FunctionsLM.R
+++ FunctionsLM.R
@@ -5,9 +5,11 @@
 # seed  - starting seed value
 generateY <- function(X, beta, sigma, seed = 5832652){
   #[ToDo] Set seed and generate Y following linear model
-  
   set.seed(seed)
+  # generate eps of the same length as Y
   eps = rnorm(dim(X)[1], 0, sigma)
+  
+  # Generate Y from args
   Y = X %*% beta + eps
   # Return Y
   return(Y)
@@ -18,14 +20,15 @@ generateY <- function(X, beta, sigma, seed = 5832652){
 # Y -response
 calculateBeta <- function(X, Y){
   # Calculate beta_LS
-  beta_LS = solve(t(X) %*% X) %*% (t(X) %*% Y)
+  beta_LS = solve((t(X) %*% X), (t(X) %*% Y)) 
   # Return beta
   return(beta_LS)
 }
 
 # Calculate MSE
 calculateMSE <- function(beta, beta_LS){
-  MSE = sqrt(sum((beta - beta_LS) ^ 2))
+  # Calculate MSE
+  MSE = sqrt(crossprod(beta - beta_LS))
   # Return MSE - error ||beta - beta_LS||_2^2
   return(MSE)
 }
